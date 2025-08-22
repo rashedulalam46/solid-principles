1f4915d76547165060d2ea4d104685cb12067f4c
Rashedul Alam <rashedulalam46@gmail.com>
2025-08-22 19:26:00 +0600

    first comint

--- solid-principles/Application/CheckoutService.cs
+++ solid-principles/Application/CheckoutService.cs
@@ -1,11 +1,25 @@
+using SolidPrinciples.Domain;
+using SolidPrinciples.Infrastructure;
+
 namespace Application;
 
 public class CheckoutService
 {
-    public CheckoutService()
+    private readonly IPaymentProcessor _paymentProcessor;
+    private readonly IInvoiceGenerator _invoiceGenerator;
+    private readonly OrderRepository _orderRepository;
+
+    public CheckoutService(IPaymentProcessor paymentProcessor, IInvoiceGenerator invoiceGenerator, OrderRepository orderRepository)
     {
-        // Constructor logic here
+        _paymentProcessor = paymentProcessor;
+        _invoiceGenerator = invoiceGenerator;
+        _orderRepository = orderRepository;
     }
 
-    // Add methods and properties as needed
+    public void Checkout(Order order)
+    {
+        _paymentProcessor.ProcessPayment(order);
+        _invoiceGenerator.GenerateInvoice(order);
+        _orderRepository.Save(order);
+    }
 }

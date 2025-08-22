f426890f3a870074418b7497b03b06204feaa021
Rashedul Alam <rashedulalam46@gmail.com>
2025-08-22 19:29:02 +0600

    impplimnt main funciotn

--- solid-principles/Program.cs
+++ solid-principles/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Application;
+using SolidPrinciples.Domain;
+using SolidPrinciples.Infrastructure;
+
+Order order = new Order { OrderId = "123", Amount = 100.50m };
+
+// Plug in any payment processor & invoice generator
+IPaymentProcessor payment = new PayPalPayment();
+IInvoiceGenerator invoice = new PdfInvoice();
+
+CheckoutService checkout = new CheckoutService(payment, invoice, new OrderRepository());
+checkout.Checkout(order);
